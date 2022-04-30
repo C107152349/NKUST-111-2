23024d2cafcfcc060160ca5b2426811025adc61a
C107152349 <C107152349@nkust.edu.tw>
2022-04-30 21:04:08 +0800

    update js

--- index.js
+++ index.js
@@ -1,22 +1,37 @@
 const readdata = document.getElementById("readdata");
 const select = document.getElementById("select_year");
-
+const btn = document.getElementById("btn");
+let data_global;
+let map;
+initmap();
 select.addEventListener('change', () => {
     const data_url = "./data/102_110/" + select.value + ".json";
-    fetch(data_url)
-        .then(res => {
-            let data = res.json();
-            return data;
-        })
-        .then(data => {
-            //console.log(data);
-            readdata.innerHTML = update(data);
-    })
+    const xhr = new XMLHttpRequest;
+    xhr.open("get", data_url, true);
+    xhr.send();
+    xhr.onload = function () {
+        data_global = JSON.parse(xhr.responseText);
+        readdata.innerHTML = update(data_global);
+    }
+    
 })
+btn.addEventListener('click', () => {
+    console.log(data_global);
+})
+function initmap() {
+    let mapOptions = {
+        center: new google.maps.LatLng(25.04674, 121.54168),
+        zoom: 13,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    const map_url = "./data/TOWN_MOI_1100415.json";
+    let mapElement = document.getElementById("mapDiv");
+    map = new google.maps.Map(mapElement, mapOptions);
+    map.data.loadGeoJson(map_url);
+}
 function update(data){
     let htmlstr = ``;
     data.forEach(element => {
-        console.log(element);
         htmlstr = htmlstr + `
         <div align ="center">
             <p> 
