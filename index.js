7da31c73689b6b186bc406da65f66c7301e000f4
C107152349 <C107152349@nkust.edu.tw>
2022-05-24 23:27:28 +0800

    new pick site function

--- index.js
+++ index.js
@@ -12,10 +12,9 @@ let tiles = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}
     minZoom: 8,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
         'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    id: 'mapbox/light-v9',
-    tileSize: 512,
-    zoomOffset: -1
+    id: 'mapbox/light-v9'
 }).addTo(map);
+let draw_the_site = [L.polygon([]).addTo(map)]; //初始化，用來框出選擇的地區
 function getColor(d) {
     return d > 40000 ? '#800026' :
            d > 3000  ? '#BD0026' :
@@ -31,11 +30,19 @@ function first_draw() {
     req.open('GET', './data/TOWN_MOI_1100415.json');
     req.send()
     req.onload = function () {
-        let obj = JSON.parse(req.responseText);
-        let geoJson = L.geoJson(obj, { "color": "#100", "weight": 0.5, "opacity": 0.9 });
+        obj = JSON.parse(req.responseText);
+        let geoJson = L.geoJson(obj,{ "color": "#100", "weight": 0.5, "opacity": 0.9 ,onEachFeature:onEachFeature});
         geoJson.addTo(map);
         geoJsons_global.push(geoJson);
     }
+}
+function onEachFeature(feature, layer) {
+    layer.on('click', function (e) {
+        let s = Object;
+        s.value = feature.properties.COUNTYNAME + feature.properties.TOWNNAME;
+        pick_site(s);
+        popup(e);
+    })
 }
 function draw(data) {
         obj = JSON.parse(req.responseText);
@@ -52,7 +59,8 @@ function draw(data) {
             "fillColor": getColor(data.find(x => x.site_id === site).population_density),
             "color": "#100",
             "weight": 0.5,
-            "opacity": 0.9
+            "opacity": 0.9,
+            onEachFeature:onEachFeature
             };
             let geoJson = L.geoJson(element, myStyle);
             geoJson.addTo(map);
@@ -88,6 +96,7 @@ select.addEventListener('change', () => {
             show_site();
         }
     })
+    document.getElementById("info").innerHTML = "";
 })
 function update(data){
     let htmlstr = ``;
@@ -108,9 +117,59 @@ function update(data){
 }
 function autocomplete(inp, site_names) {
 }
-function pick_site(obj) {
-    
-    console.log(obj.value);
+function pick_site(s) {
+    console.log(s.value);
+    try {
+        obj.features.forEach(element => {
+            let site = element.properties.COUNTYNAME + element.properties.TOWNNAME;
+            if (s.value == site) {
+                draw_the_site.forEach(d => {
+                    map.removeLayer(d);
+                })
+                draw_the_site = [];
+                let site_loca_oldarr = element.geometry.coordinates;
+                let site_loca_newarr = [];
+                site_loca_oldarr.forEach(arr => {
+                    if (site_loca_oldarr.length == 1) {
+                        site_loca_newarr.push(rev(arr));
+                    }
+                    else if (site_loca_oldarr.length >= 1) {
+                        site_loca_newarr.push(rev(arr[0]));
+                    }
+                })
+                site_loca_newarr.forEach(arr => {
+                    draw_the_site.push(L.polygon([arr], {}).addTo(map));
+                })
+            }
+            
+        });
+    } catch {}
+}
+function popup(e) {
+    let site = e.sourceTarget.feature.properties.COUNTYNAME + e.sourceTarget.feature.properties.TOWNNAME;
+    try {
+        let site_data = data.find(x => x.site_id === site);
+        let year = site_data.statistic_yyy;
+        let people_total = site_data.people_total;
+        let area = site_data.area;
+        let population_density = site_data.population_density;
+        let content = "民國 : " + year + " 年</br>"
+            + site + "</br>" + "總人口 : " + people_total + " 人</br>"
+            + "面積 : " + area + " 平方公里</br>" + "人口密度 : " + population_density + " 人/km^2";
+        console.log(content);
+        let popup = L.popup();
+        popup
+            .setLatLng(e.latlng)
+            .setContent(content)
+            .openOn(map);
+    } catch { 
+        let content = site;
+        let popup = L.popup();
+                    popup
+                        .setLatLng(e.latlng)
+                        .setContent(content)
+                        .openOn(map);
+    }
 }
 function show_site() {
     let htmlstr;
@@ -120,8 +179,18 @@ function show_site() {
         return;
     htmlstr = site_class(data, inp_site);
     site_info.innerHTML = htmlstr;
-    // obj.features.forEach(element => {
-    //     console.log(element.properties);
-    // });
+
+}
+function rev(arr) {
+    let new_arr = [];
+    let new_arr2 = [];
+    for (i = 0; i < arr.length; i = i + 1){
+        new_arr2 = [];
+        for (j = arr[i].length-1; j >= 0; j = j - 1){
+            new_arr2.push(arr[i][j]);
+        }
+        new_arr.push(new_arr2);
+    }
+    return new_arr;
 }
 console.log("搜尋'all'可列出所有地區");
